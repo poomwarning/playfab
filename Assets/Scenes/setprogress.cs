85fbb6b4f0930ef146c165d25e0d8d5208e30022
PEPSIWOOD <35100830+poomwarning@users.noreply.github.com>
2021-11-23 17:35:20 +0700

    json test

--- Assets/Scenes/setprogress.cs
+++ Assets/Scenes/setprogress.cs
@@ -9,7 +9,11 @@ public class Progress
     public string name;
     public string rank;
     public float year1;
+    public List<stagestatfuck> statge ;
+    public Dictionary<string ,string> dicktest ;
+    public int [] arrayfucked;
     public float accurate;
+
     public Progress(string name,string rank,float year1,float accurate)
     {
         this.name = name;
@@ -30,12 +34,21 @@ public class setprogress : MonoBehaviour {
     public Progress ReturnClass(){
         return new Progress(nameInput.text,rankInput.text,float.Parse(year1progress.text),float.Parse(accurate.text));
     }
+    private void Start() 
+    {
+        /*  var oof = new Progress();
+        oof.stagestudentstat.namefuck = "1-2";
+         oof.stagestudentstat.accfuck = 100;
+         oof.stagestudentstat.time = 60;*/
+    }
     public void SetUi(Progress progress)
     {
         nameInput.text = progress.name;
         rankInput.text =  progress.rank;
         year1progress.text = progress.year1.ToString();
         accurate.text = progress.accurate.ToString();
+      
+        
     
     }
 }
---
+++ Assets/Scenes/stagestatfuck.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class stagestatfuck 
+{
+    public string namefuck;
+    public float accfuck;
+    public int time ;
+
+}
+
